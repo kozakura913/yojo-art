4fe461af4c8385e7f21ff34ca469e095f6a812e6
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2024-06-28 08:34:18 +0900

    add

--- packages/frontend/src/pages/official-tag.vue
+++ packages/frontend/src/pages/official-tag.vue
@@ -1,5 +1,5 @@
 <!--
-SPDX-FileCopyrightText: syuilo and misskey-project
+SPDX-FileCopyrightText: syuilo and misskey-project, yojo-art team
 SPDX-License-Identifier: AGPL-3.0-only
 -->
 
@@ -9,7 +9,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 
 	<MkSpacer :contentMax="500">
 		<div class="_gaps">
-			<MkOfficialTag v-for="tag in official_tags" :key="tag.tag" :tagString="tag.tag"/>
+			<MkOfficialTag v-for="tag in official_tags" :key="tag.tag" :tagString="tag.tag" :description="tag.description" :bannerUrl="tag.bannerUrl"/>
 		</div>
 	</MkSpacer>
 </MkStickyContainer>
@@ -22,9 +22,18 @@ import { i18n } from '@/i18n.js';
 const official_tags = [
 	{
 		tag: 'test',
+		description: 'テストタグの説明文',
+		bannerUrl: 'https://misskey-files.kzkr.xyz/files/065f42e8-f03b-4c89-96a7-ec8b23650de0.png',
 	},
 	{
 		tag: 'aaaa',
+		description: 'あああああああああ',
+		bannerUrl: null,
+	},
+	{
+		tag: 'にゃーん',
+		description: 'にゃああああああ',
+		bannerUrl: null,
 	},
 ];
 
