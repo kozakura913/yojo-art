b5d38cf4a617a0b8c31b3dff1428ea848137e7ef
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2024-11-16 01:45:36 +0900

    wip

--- packages/frontend-shared/js-built/collapsed.d.ts
+++
@@ -1,5 +0,0 @@
-import * as Misskey from 'cherrypick-js';
-export declare function shouldCollapsed(note: Misskey.entities.Note, urls: string[]): boolean;
-export declare function shouldMfmCollapsed(note: Misskey.entities.Note): boolean;
-export declare function shouldAnimatedMfm(note: Misskey.entities.Note): boolean;
-//# sourceMappingURL=collapsed.d.ts.map
--- packages/frontend-shared/js-built/extract-avg-color-from-blurhash.js
+++
@@ -1,7 +0,0 @@
-function extractAvgColorFromBlurhash(hash) {
-  return typeof hash === "string" ? "#" + [...hash.slice(2, 6)].map((x) => "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#$%*+,-.:;=?@[]^_{|}~".indexOf(x)).reduce((a, c) => a * 83 + c, 0).toString(16).padStart(6, "0") : void 0;
-}
-export {
-  extractAvgColorFromBlurhash
-};
-//# sourceMappingURL=extract-avg-color-from-blurhash.js.map
