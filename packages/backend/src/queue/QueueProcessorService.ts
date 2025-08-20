7ab676f06ee42b1652f206b17d830353daf882d6
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2025-08-20 14:36:26 +0900

    fix

--- packages/backend/src/queue/QueueProcessorService.ts
+++ packages/backend/src/queue/QueueProcessorService.ts
@@ -521,7 +521,7 @@ export class QueueProcessorService implements OnApplicationShutdown {
 
 		//#region schedule note post
 		this.schedulerNotePostQueueWorker = new Bull.Worker(QUEUE.SCHEDULE_NOTE_POST, (job) => this.scheduleNotePostProcessorService.process(job), {
-			...baseQueueOptions(this.config, QUEUE.SCHEDULE_NOTE_POST, this.redisForJobQueue),
+			...baseWorkerOptions(this.config, QUEUE.SCHEDULE_NOTE_POST, this.redisForJobQueue),
 			autorun: false,
 		});
 		//#endregion
