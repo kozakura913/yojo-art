62853072beabd33138e7ce63371a317cb7abfe19
4sterisk <4sterisk@ymail.ne.jp>
2026-01-02 05:49:16 +0900

    fix: event

--- packages/cherrypick-js/src/autogen/types.ts
+++ packages/cherrypick-js/src/autogen/types.ts
@@ -4730,7 +4730,18 @@ export type components = {
             emojis?: {
                 [key: string]: string;
             };
-            event?: Record<string, never> | null;
+            event?: {
+                title: string;
+                /** Format: date-time */
+                start: string;
+                /** Format: date-time */
+                end: string | null;
+                metadata?: {
+                    '@type'?: string;
+                    location?: string;
+                    description?: string;
+                };
+            } | null;
             /**
              * Format: id
              * @example xxxxxxxxxx
@@ -6060,7 +6071,11 @@ export type components = {
                 start: string;
                 /** Format: date-time */
                 end: string | null;
-                metadata: Record<string, never>;
+                metadata?: {
+                    '@type'?: string;
+                    location?: string;
+                    description?: string;
+                };
             } | null;
             fileIds?: string[];
             files?: components['schemas']['DriveFile'][];
